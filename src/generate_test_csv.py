8f5649d5742f33f28b8002a6f5a00adad6153056
tginsbu1 <tsg44@cornell.edu>
2025-12-18 08:36:05 -0500

    test

--- src/generate_test_csv.py
+++ src/generate_test_csv.py
@@ -5,13 +5,15 @@ columns = ["Source_Plate", "Source_Well", "Dest_Plate", "Dest_Well", "Transfer_V
 output_data = pd.DataFrame(columns=columns)
 source_plates = ["A1", "A2", "A3"]
 destination_plate = "B1"
+well_volume = 250
 num_reagents = 17
 num_samples = 18
+max_volume = int(well_volume/num_reagents)
 for i in range(num_reagents):
     source_well = f"{chr(65 + (i % 8))}{(i // 8) + 1}"
     for j in range(num_samples):
         dest_well = f"{chr(65 + (j % 8))}{(j // 8) + 1}"
-        volume = random.randrange(1, 300)
+        volume = random.randrange(1, max_volume)
         source_plate = source_plates[i // 8]
         output_data.loc[len(output_data)] = [source_plate,source_well,destination_plate, dest_well,volume]
         
